a4425dae0141ca32a8cfafe635b4c61494779406
Randy <randymotaj@gmail.com>
2021-02-05 12:05:12 -0400

    Js example

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,66 @@
 const prev = document.getElementById('prevBtn');
 const next = document.getElementById('nextBtn');
+const heroImg = document.getElementById('hero');
+const heroTitle = document.getElementById('hero-description-title');
+const heroText = document.getElementById('hero-description-text');
 
-next.addEventListener('click', slider);
+const profiles = [
+    one = {
+        img: `./src/images/desktop-image-hero-1.jpg`,
+        title: "Discover innovative ways to decorate",
+        text: `We provide quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`
+    },
+    two = {
+        img: `./src/images/desktop-image-hero-2.jpg`,
+        title: `We are available all across the globe`,
+        text: `With stores all over the world, it's easy for you to find furniture for your home or place business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`
+    },
+    three = {
+        img: `./src/images/desktop-image-hero-3.jpg`,
+        title: `Manufactured with the best materials`,
+        text: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and consistent as possible. With three decades of experience in his industry, we understand what customers want for their home and office.`
+    }
+];
 
-function slider() {};
+// next.addEventListener('click', () => {
+//     function number() {
+
+//     }
+// });
+
+next.addEventListener('click', () => {
+    var src = heroImg.getAttribute('src');
+    if(src === profiles[0].img) {
+        heroImg.setAttribute('src', `${profiles[1].img}`);
+        heroTitle.innerHTML = `${profiles[1].title}`;
+        heroText.innerHTML = `${profiles[1].text}`;
+    } else if(src === profiles[1].img) {
+        heroImg.setAttribute('src', `${profiles[2].img}`);
+        heroTitle.innerHTML = `${profiles[2].title}`;
+        heroText.innerHTML = `${profiles[2].text}`;
+    } else {
+        heroImg.setAttribute('src', `${profiles[0].img}`);
+        heroTitle.innerHTML = `${profiles[0].title}`;
+        heroText.innerHTML = `${profiles[0].text}`;
+    }
+});
+prev.addEventListener('click', () => {
+    var src = heroImg.getAttribute('src');
+    if(src === profiles[0].img) {
+        heroImg.setAttribute('src', `${profiles[2].img}`);
+        heroTitle.innerHTML = `${profiles[2].title}`;
+        heroText.innerHTML = `${profiles[2].text}`;
+    } else if(src === profiles[1].img) {
+        heroImg.setAttribute('src', `${profiles[0].img}`);
+        heroTitle.innerHTML = `${profiles[0].title}`;
+        heroText.innerHTML = `${profiles[0].text}`;
+    } else {
+        heroImg.setAttribute('src', `${profiles[1].img}`);
+        heroTitle.innerHTML = `${profiles[1].title}`;
+        heroText.innerHTML = `${profiles[1].text}`;
+    }
+});
+
+
+
 
